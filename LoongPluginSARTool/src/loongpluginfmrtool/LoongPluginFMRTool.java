bb3351f95e670c23133d33a20b12dda040e1df4d
csytang <chris.yttang@hotmail.com>
2016-09-01 13:54:22 +0800

    small fix

--- LoongPluginSARTool/src/loongpluginfmrtool/LoongPluginFMRTool.java
+++ LoongPluginSARTool/src/loongpluginfmrtool/LoongPluginFMRTool.java
@@ -10,7 +10,7 @@ import org.osgi.framework.BundleContext;
 public class LoongPluginFMRTool extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "LoongPlugin.SARTool"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "LoongPlugin.FMRTool"; //$NON-NLS-1$
 
 	// The shared instance
 	private static LoongPluginFMRTool plugin;
