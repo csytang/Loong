cd27d713b8e717217f60bbb34d2097a1edd9cc0c
csytang <chris.yttang@hotmail.com>
2016-10-25 09:12:02 +0800

    update

--- LoongPluginSARTool/src/loongpluginfmrtool/toolbox/mvs/ModuleVariabilitySystem.java
+++ LoongPluginSARTool/src/loongpluginfmrtool/toolbox/mvs/ModuleVariabilitySystem.java
@@ -1,6 +1,5 @@
 package loongpluginfmrtool.toolbox.mvs;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -13,33 +12,18 @@ import loongpluginfmrtool.module.model.Module;
 
 public class ModuleVariabilitySystem {
 	
-	private Map<Module,ModuledFeature>modulesToFeatures = new HashMap<Module,ModuledFeature>();
 	private Set<ModuledFeature> features = new HashSet<ModuledFeature>();
 	private ModuleBuilder builder;
 	private Map<Integer, Module>indexToModule = new HashMap<Integer, Module>();
-	private Map<Module, Integer>ModuleToIndex = new HashMap<Module, Integer>();
-	private Map<Integer, ModuledFeature> indexToFeature = new HashMap<Integer, ModuledFeature>();
-	
 	private Set<Module> allmodules = new HashSet<Module>();
-	private ModuleDependencyTable dependency_table;
-	private double[][] information_loss_table_array;
-	private double[][] variability_loss_table_array;
-	private int[][] moduledependency_table;
-	private int[][] featuredependency_table_array;
-	private double[][] featuredependency_table_normalized_array;
-	private int size;
-	private int totalsize;
-	private double MAXVALUE = 1;
+	private ModuleDependencyTable dependency_table;	
 	private int cluster;
-	private boolean debug = true;
-	private double variabilitythreshold = 0.3;
 	public ModuleVariabilitySystem(ModuleBuilder pbuilder,int pcluster){
 		this.builder = pbuilder;
 		this.cluster = pcluster;
 		this.indexToModule = this.builder.getIndexToModule();
 		this.allmodules = new HashSet<Module>(this.indexToModule.values());
 		this.dependency_table = this.builder.getDependencyTable();
-		this.totalsize = this.indexToModule.size();
 		initialize();
 		performClustering();
 	}
